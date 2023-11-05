456f76d6d8c320c632955e971abe8c057d11036b
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-05 18:43:36 +0800

    fix useQuery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
   const { searchTerm } = useGlobalContext();
 
   const { isLoading, isError, data } = useQuery({
-    queryKey: ['images'],
+    queryKey: ['images', searchTerm],
     queryFn: async () => {
       const { data } = await axios.get(`${url}&query=${searchTerm}`);
       return data;
